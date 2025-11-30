ab86803c67bc64752966a51e0ebc23912a4e35c8
VGeroski <vlada.geroski@gmail.com>
2025-11-30 22:14:31 +0100

    Dodavanje druge verzije rekurzije

--- 05_vezbe/z3.c
+++ 05_vezbe/z3.c
@@ -5,6 +5,8 @@
 
 #include <stdio.h>
 
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
 int najveca_cifra(int broj)
 {
     if (broj < 10)
@@ -15,13 +17,25 @@ int najveca_cifra(int broj)
         int max_cifra = najveca_cifra(broj / 10); /* maksimalna cifra do poslednje cifre */
         /* ostaje da poredimo samo jos sa poslednjom cifrom */
         int trenutna_cifra = broj % 10;
-        return (trenutna_cifra > max_cifra ? trenutna_cifra : max_cifra);
+        return MAX(trenutna_cifra, max_cifra);
     }
+}
+
+int najveca_cifra_v2(int broj)
+{
+    if (broj < 10)
+        return broj;
+
+    int trenutna_cifra = broj % 10;
+    int max_cifra = najveca_cifra_v2(broj / 10);
+
+    return MAX(trenutna_cifra, max_cifra);
 }
 
 int main()
 {
     printf("Najveca cifra %d\n", najveca_cifra(23451));
+    printf("Najveca cifra (v2) %d\n", najveca_cifra_v2(23451));
 
     return 0;
 }
